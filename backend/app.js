dc70460d4b70d8195de57f1721a9185aa1b31406
LJ-Jones <admin@Jane.broadband>
2021-03-11 15:44:02 +0000

    corrected error in serverjs & appjs

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const fileUpload = require('express-fileUpload');
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileUpload')
 //const dotenv = require('dotenv');
 const path = require('path')
 
-const errorMiddleware  = require('./middlewares/errors');
+const errorMiddleware  = require('./middlewares/errors')
 
 // Setting up config file
 if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../front/build/index.html'))
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
     })
 }
 
--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,9 @@ connectDatabase();
 
 // Setting up cloudinary configuration
 cloudinary.config({
-    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
-    api_key : process.env.CLOUDINARY_API_KEY,
-    api_secret : process.env.CLOUDINARY_API_SECRET
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
 const server = app.listen(process.env.PORT, () => {
