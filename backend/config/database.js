f64f4ee74e00b9adf2e7681b98b8d306bc282714
LJ-Jones <admin@Jane.broadband>
2021-03-11 09:58:31 +0000

    initial commit

---
+++ backend/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }).then(con => {
+        console.log(`mongoDB Database connected with HOST: ${con.connection.host}`)
+    })
+}
+
+module.exports = connectDatabase;
